b19910375c49d437e36208bec0edbfcb1c177bf5
Christian <christianvillere@gmail.com>
2023-02-01 20:24:14 -0800

    Add #make_board To create the board game will be played on.

--- main.rb
+++ main.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
-require 'pry'
+# require 'pry'
 
+class GameBoard
 
+  attr_accessor :board
+
+  def initialize(board = [])
+    @board = board
+  end
+
+  def make_board
+    6.times do
+      @board.push(['[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'])
+    end
+  end
+  
+end
