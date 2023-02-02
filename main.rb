3a80f2e1e3b19f64e582c511f3fc4d2043412fd2
Christian <christianvillere@gmail.com>
2023-02-01 21:32:07 -0800

    Add display_board method So we can display the Gameboard after each move to the players

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ class GameBoard
 
   attr_accessor :board
 
-  def initialize(board = [])
+  def initialize(board)
     @board = board
   end
 
@@ -14,6 +14,29 @@ class GameBoard
     6.times do
       @board.push(['[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]', '[ ]'])
     end
+    @board
   end
-  
+
+  def display_board
+    @board = @board.each do |x|
+      puts x.join('')
+    end
+  end
+end
+
+#flow
+my_board = GameBoard.new()
+board_structure = my_board.make_board
+board_display = my_board.display_board
+board_display
+
+module GameRules
+
+  def show_game_rules
+    puts 'Select a row and column to put your letter (X) for player 1 and 
+    (O) for player 2. The space must either be the lowest row or on top of
+    existing moves at given spaces. New selections will not replace prior
+    selections'
+  end
+
 end
