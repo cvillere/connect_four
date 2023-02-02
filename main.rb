9c6d331746548ec2d0fd6232e7f8459878ea580d
Christian <christianvillere@gmail.com>
2023-02-01 21:45:39 -0800

    Add GameRules module So we can display to players the rules of the Game at certain points during game play

--- main.rb
+++ main.rb
@@ -30,13 +30,26 @@ board_structure = my_board.make_board
 board_display = my_board.display_board
 board_display
 
+#holds rules to
 module GameRules
 
   def show_game_rules
     puts 'Select a row and column to put your letter (X) for player 1 and 
-    (O) for player 2. The space must either be the lowest row or on top of
-    existing moves at given spaces. New selections will not replace prior
-    selections'
+    (O) for player 2. The space chosen must either be the lowest row or on top of
+    existing moves at given spaces. There CANNOT be a gap in rows between moves
+    already made and a new move. New selections will not replace prior
+    selections.'
+  end
+
+  def show_win_criteria
+    puts 'A player wins when 4 of their letters are consecutively connected either
+    vertically, horizontally, or diagonally'
+  end
+
+  def show_move_rules
+    puts "1.Space chosen must be the lowest row or top of existing moves. 
+    Can't have gap in rows. 
+    2. Space chosen must be available as new moves will not replace old moves."
   end
 
 end
